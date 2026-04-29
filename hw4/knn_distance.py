d6987be013b39a6a4ce80140f6bd6c50b4118944
Jingyun Xiong <jing@Mac.lan>
2026-04-29 14:28:38 -0700

    finish knn_distance

--- hw4/knn_distance.py
+++ hw4/knn_distance.py
@@ -3,5 +3,8 @@ import random
 def knn_distance(arr, q, k):
     """Compute the kth nearest point and the distance to it."""
     # TODO: Implement the knn_distance function
-
-    return None
+    distances = []
+    for x in arr:
+        distances.append((abs(x - q), x))
+    distances.sort()
+    return distances[k-1]
