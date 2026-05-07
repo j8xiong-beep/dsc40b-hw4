0877b2617113666fd311ffc3306a97c80b1b26ee
j8xiong-beep <j8xiong@ucsd.edu>
2026-05-06 21:50:13 -0700

    Delete knn_distance.py

--- knn_distance.py
+++
@@ -1,9 +0,0 @@
-import random
-def knn_distance(arr, q, k):
-    """Compute the kth nearest point and the distance to it."""
-    # TODO: Implement the knn_distance function
-    distances = []
-    for x in arr:
-        distances.append((abs(x - q), x))
-    distances.sort()
-    return distances[k-1]
